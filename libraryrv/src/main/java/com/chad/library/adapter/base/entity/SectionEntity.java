c4de38b9c566fca8efd310aa366bdae653ee50e6
880414ytg <995278340@qq.com>
2018-07-10 14:14:28 +0800

    第一次提交

---
+++ libraryrv/src/main/java/com/chad/library/adapter/base/entity/SectionEntity.java
@@ -0,0 +1,27 @@
+package com.chad.library.adapter.base.entity;
+
+import java.io.Serializable;
+
+/**
+ * https://github.com/CymChad/BaseRecyclerViewAdapterHelper
+ */
+public abstract class SectionEntity<T> implements Serializable {
+    public boolean isHeader;
+    public T t;
+    public String header;
+
+    public SectionEntity(boolean isHeader, String header) {
+        this.isHeader = isHeader;
+        this.header = header;
+        this.t = null;
+    }
+
+    public SectionEntity(T t, String header) {
+
+
+    }
+
+    public SectionEntity(T t) {
+        this.t = t;
+    }
+}
